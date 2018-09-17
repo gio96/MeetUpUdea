2b3881b104899e516dfef9d6fefc8a0c8a1b5aa7
gio96 <gio96gomez@gmail.com>
2018-09-17 18:34:35 -0500

    Tabs

--- app/src/main/java/com/example/galonsogomez/meetupudea/ShowGroupActivity.java
+++ app/src/main/java/com/example/galonsogomez/meetupudea/ShowGroupActivity.java
@@ -3,12 +3,15 @@ package com.example.galonsogomez.meetupudea;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ShowGroupActivity extends AppCompatActivity {
 
-
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,18 @@ public class ShowGroupActivity extends AppCompatActivity {
         ShowGroupFragment showGroupFragment = new ShowGroupFragment();
         //Se le envian los datos al fragment
         showGroupFragment.setArguments(b);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        //add Fragments
+        adapter.addFragment(new ShowGroupEventsFragment(), "Upcoming");
+        adapter.addFragment(new ShowGroupInforFragment(), "Information");
+        adapter.addFragment(new ShowGroupCoursesFragment(), "Courses");
+        //adapter setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 }
