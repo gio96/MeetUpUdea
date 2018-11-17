ad651e42ad8117404777a3c12ea86a97a180dd57
gio96 <gio96gomez@gmail.com>
2018-11-17 13:24:48 -0500

    ChangesShowEvent

--- app/src/main/java/com/example/galonsogomez/meetupudea/ShowEventActivity.java
+++ app/src/main/java/com/example/galonsogomez/meetupudea/ShowEventActivity.java
@@ -1,9 +1,7 @@
 package com.example.galonsogomez.meetupudea;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
